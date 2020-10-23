318a50a4f416ed527e29715e5974b73805337028
Theelipan prabakar <theelipan@gmail.com>
2020-10-22 23:33:20 -0400

    add fallback poster if api poster is null

--- src/components/DisplayBtnResult.js
+++ src/components/DisplayBtnResult.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import { ReactComponent as Logo } from './TMDB_LOGO.svg';
 
 function DisplayBtnResult({ result }) {
   //   const posterUrl = 'https://image.tmdb.org/t/p/w1280/';
-  const posterUrl =
-    result.poster_path !== null
-      ? `https://image.tmdb.org/t/p/w220_and_h330_face/${result.poster_path}`
-      : `https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg`;
-  console.log(posterUrl);
+  const isPosterUrlvalid = result.poster_path !== null ? `https://image.tmdb.org/t/p/w220_and_h330_face/${result.poster_path}` : require('./TMDB_LOGO.svg');
 
   return (
     <div className='card'>
-      <img src={`${posterUrl}`} className='card-poster'></img>
+      <img src={`${isPosterUrlvalid}`} className='card-poster'></img>
       <div className='card-release_date'>{result.release_date.split('-')[0]}</div>
       <div className='card-title'>{result.title}</div>
     </div>
