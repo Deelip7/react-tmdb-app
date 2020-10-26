56bcb1737ba39e91b3e7143e4c551ec5fa826079
Theelipan prabakar <theelipan@gmail.com>
2020-10-26 19:43:49 -0400

    chnage popup layout and movie backdrop

--- src/components/DisplayMovieDetails.js
+++ src/components/DisplayMovieDetails.js
@@ -1,31 +1,37 @@
 import React from 'react';
 
 function DisplayMovieDetails({ movieDetails, closeMovieDetails }) {
-  let isPosterUrlvalid = movieDetails.poster_path !== null ? `https://image.tmdb.org/t/p/w220_and_h330_face/${movieDetails.poster_path}` : require('./svgs/TMDB_LOGO2.svg');
+  let defaultPosterUrl = require('./svgs/TMDB_LOGO2.svg');
+  let posterUrl = `https://image.tmdb.org/t/p/w220_and_h330_face`;
+
+  const { title, poster_path, backdrop_path, overview, release_date, runtime, homepage, genres } = movieDetails;
+
+  let moviePoster = poster_path !== null ? `${posterUrl}${poster_path}` : defaultPosterUrl;
+  let movieBackdrop = backdrop_path !== null ? `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${backdrop_path}` : defaultPosterUrl;
+
+  let genresList = genres
+    .map((genre) => {
+      return `${genre.name}`;
+    })
+    .join(', ');
 
   return (
     <div className='movieDetail-container'>
-      <div onClick={closeMovieDetails} className='movieDetail-close'>
-        <span>X</span>
-      </div>
-      <div>
-        <img src={`${isPosterUrlvalid}`} className='movieDetail-poster' alt={`${movieDetails.title} Poster`}></img>
+      <div className='movieDetail-backdrop-container'>
+        <img src={movieBackdrop} className='movieDetail-backdrop' alt={`${movieDetails.title} Poster`}></img>
       </div>
       <div className='movieDetail'>
-        <div className='movieDetail-title'>{movieDetails.title}</div>
-        <div className='movieDetail-genres'>
-          {movieDetails.genres.map((e) => {
-            console.log(e.name);
-            return `${e.name}  `;
-          })}
+        <div onClick={closeMovieDetails} className='movieDetail-close'>
+          <span>X</span>
         </div>
-
-        <div>{`Release date: ${movieDetails.release_date}`}</div>
-        <div>{`${movieDetails.runtime} min`}</div>
+        <img src={`${moviePoster}`} className='movieDetail-poster' alt={`${title} Poster`}></img>
         <div>
-          <a href={`${movieDetails.homepage}`}>{`Home Page`}</a>
+          <div className='movieDetail-title'>{title}</div>
+          <div className='movieDetail-genres'>{`Genres: ${genresList} |  ${runtime} min`}</div>
+          <div>{`Release date: ${release_date}`}</div>
         </div>
-        <div className='movieDetail-overview'>{movieDetails.overview}</div>
+        {/* <a href={`${homepage}`}>{`Home Page`}</a> */}
+        <div className='movieDetail-overview'>{overview}</div>
       </div>
     </div>
   );
